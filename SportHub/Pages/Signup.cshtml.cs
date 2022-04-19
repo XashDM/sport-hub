6947e847ddc5439b380a2869e76263de8d25f1cd
Ihor <ihor.toporivskyi@gmail.com>
2022-04-20 00:27:26 +0300

    Implemented form value passing to controller

--- SportHub/Pages/Signup.cshtml.cs
+++ SportHub/Pages/Signup.cshtml.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SportHub.Models;
+using SportHub.Services;
 
 namespace SportHub.Pages
 {
     public class SignupModel : PageModel
     {
+        private readonly IUserService _userService;
+
+        public SignupModel(IUserService userService)
+        {
+            _userService = userService;
+        }
         [BindProperty]
         public SignupCredentials signupCredentials { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost([FromBody] SignupCredentials credentials)
+        {
+            _userService.CreateUser(credentials.Email, credentials.PasswordHash, credentials.FirstName, credentials.LastName);
+
+            return RedirectToPage("Index");
         }
     }
 }
--- SportHub/wwwroot/js/site.js
+++ SportHub/wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+const { error } = require("jquery");
+
 // Write your JavaScript code.
 
 
@@ -10,4 +12,39 @@ async function sha256(message) {
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
+}
+
+$("#signup-form").submit((event) => {
+    event.preventDefault();
+
+    const firstName = $("#firstname-input").val().toString();
+    const lastName = $("#lastname-input").val().toString();
+    const emailAddress = $("#email-input").val().toString();
+
+    let passwordHash = sha256($('#password-input').val().toString()).then((res) => {
+        passwordHash = res;
+        SignUp(firstName, lastName, emailAddress, passwordHash);
+    });
+})
+
+function SignUp(firstName, lastName, email, passwordHash) {
+    $.ajax({
+        headers:
+        {
+            "RequestVerificationToken": $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+        async: false,
+        url: '/Signup',
+        type: 'post',
+        data: {
+            'FirstName': firstName,
+            'LastName': lastName,
+            'Email': email,
+            'PasswordHash': passwordHash
+        }
+    });
+};
+
+function successCallback(responce) {
+    localStorage.setItem("efqf", responce);
 }
