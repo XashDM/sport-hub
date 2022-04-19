7d2f7fd6b3a8e6bad7fbefee6cd2ebe6a4d179d0
Ihor <ihor.toporivskyi@gmail.com>
2022-04-19 12:01:25 +0300

    Naming, Using Statements and Redundant spaces Fixed

--- SportHub.Services/Exceptions/UserServiceExceptions/EmailAlreadyInUseException.cs
+++ SportHub.Services/Exceptions/UserServiceExceptions/EmailAlreadyInUseException.cs
@@ -1,9 +1,4 @@
 ﻿using SportHub.Services.Exceptions.RootExceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SportHub.Services.Exceptions.UserServiceExceptions
 {
