1b879b8651e3631d3f9a5ec2afb8416f7cbf4bb0
annnnnet <anna.horbova.knm.2020@lpnu.ua>
2022-04-21 22:02:38 +0300

    Created Login with validation

---
+++ SportHub/Pages/Login.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SportHub.Config.JwtAuthentication;
+using Microsoft.EntityFrameworkCore;
+using SportHub.Models;
+using SportHub.Services;
+
+namespace SportHub.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<LoginModel> _logger;
+        private readonly IUserService _userService;
+        private readonly IJwtSigner _jwtSigner;
+
+        [BindProperty]
+        public LoginCredentials loginCredentials { get; set; }
+        public LoginModel(ILogger<LoginModel> logger, IUserService userService, IJwtSigner jwtSigner)
+        {
+            _logger = logger;
+            _userService = userService;
+            _jwtSigner = jwtSigner;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost(LoginCredentials loginCredentials)
+        { 
+            if (!_userService.IsExistingEmail(loginCredentials.Email))
+            {
+                return BadRequest("Invalid email");
+            }
+
+            var currentUser = _userService.GetUserByEmail(loginCredentials.Email);
+
+            if (ModelState.IsValid)
+            {
+                if (!loginCredentials.PasswordHash.Equals(currentUser.PasswordHash))
+                {
+                    return BadRequest("Invalid password");
+                }
+                
+            }
+              var token = _jwtSigner.FetchToken(currentUser);
+              return Content(token);
+        }
+    }
+}
--- SportHub/wwwroot/js/site.js
+++ SportHub/wwwroot/js/site.js
@@ -2,3 +2,49 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+$("#loginForm").submit((event) => {
+    event.preventDefault();
+
+    const emailAddress = $("#field_email").val().toString();
+
+    let passwordHash = sha256($('#field_password').val().toString()).then((res) => {
+        passwordHash = res;
+        Login(emailAddress, passwordHash);
+    });
+})
+
+function Login(email, passwordHash) {
+    $.ajax({
+        headers:
+        {
+            "RequestVerificationToken": $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+        async: false,
+        url: '/Login',
+        type: 'post',
+        data: {
+            'Email': email,
+            'PasswordHash': passwordHash
+        },
+        success(token) {
+            console.log(token);
+            localStorage.setItem('Jwt Token', token);
+            window.location.href = '/Index';
+        },
+        error(errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+};
+
+
