118d03e8d72e1f4e1dfea4994c644239bf19506d
annnnnet <anna.horbova.knm.2020@lpnu.ua>
2022-05-04 17:38:25 +0300

    corrected mistakes

--- SportHub.Services/Exceptions/UserServiceExceptions/EmailDoesntExistException.cs
+++
@@ -1,9 +0,0 @@
-﻿using SportHub.Services.Exceptions.RootExceptions;
-
-namespace SportHub.Services.Exceptions.UserServiceExceptions
-{
-    public class EmailDoesntExistException : UserServiceException
-    {
-        public EmailDoesntExistException() : base($"User with such email doesn't exists") { }
-    }
-}
