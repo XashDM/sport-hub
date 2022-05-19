4e17cffedb944ddba26ce470462a26c7230370f9
Ростислав Корнієць <Rostyslav.Korniiets.KNM.2020@edu.lpnu.ua>
2022-05-19 22:40:11 +0300

    main article section on home page

--- SportHub/Pages/Index.cshtml.cs
+++ SportHub/Pages/Index.cshtml.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SportHub.Domain.Models;
+using SportHub.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SportHub.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Article> articles = new List<Article> ();
+        public List<string> teams = new List<string> ();
+        IGetArticleService _getArticleService;
+        public int Counter = 4;
+        public IndexModel(ILogger<IndexModel> logger, IGetArticleService getArticleService)
         {
             _logger = logger;
+            _getArticleService = getArticleService;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            Article article;
+            string team = "";
+            for (int i = 4; i <= Counter+3; i++)
+            {
+                article = await _getArticleService.GetArticle(i);
+                team = _getArticleService.GetArticlesTeam(i);
+                articles.Add(article);
+                teams.Add(team);
+            }
+            return Page();
         }
     }
 }
---
+++ SportHub/wwwroot/js/homepage.js
@@ -0,0 +1,46 @@
+﻿var counter = 0;
+var mainArticles;
+var mainTeams;
+
+function setMainArticlesList(articles, teams) {
+    $("#num-0").toggleClass("red-accent");
+    mainArticles = articles;
+    mainTeams = teams;
+    console.log(mainArticles);
+}
+
+function setMainArticle() {
+    $(".main-article-photo").finish().fadeOut("5", function () {
+        $(this).attr("src", mainArticles[counter].imageLink)
+    }).fadeIn("5");
+    $("#main-article-team-tag").html("<p>" + mainTeams[counter] + "</p>");
+    $("#banner-published-date").text(mainArticles[counter].postedDate);
+    $("#banner-title").text(mainArticles[counter].title);
+    $(".main-bottom-info").text(mainArticles[counter].contentText);
+}
+
+function nextImage() {
+    $("#num-" + counter).toggleClass("red-accent");
+    if (counter < mainArticles.length - 1) {
+        counter += 1;
+    }
+    else {
+        counter = 0;
+    }
+    $("#num-" + counter).toggleClass("red-accent");
+    setMainArticle();
+    // console.log(counter);
+}
+
+function prevImage() {
+    $("#num-" + counter).toggleClass("red-accent");
+    if (counter > 0) {
+        counter -= 1;
+    }
+    else {
+        counter = mainArticles.length - 1;
+    }
+    $("#num-" + counter).toggleClass("red-accent");
+    setMainArticle();
+    // console.log(counter);
+}
