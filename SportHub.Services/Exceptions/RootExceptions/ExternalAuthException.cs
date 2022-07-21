af258e294e25adf4ca1af6bf8439805ffbb70f3d
Ihor <ihor.toporivskyi@gmail.com>
2022-07-21 12:15:10 +0300

    Fixed indentation

--- SportHub.Services/Exceptions/RootExceptions/ExternalAuthException.cs
+++ SportHub.Services/Exceptions/RootExceptions/ExternalAuthException.cs
@@ -5,6 +5,7 @@ namespace SportHub.Services.Exceptions.RootExceptions
     public abstract class ExternalAuthException : Exception
     {
         public int StatusCode { get; protected set; }
+
         protected ExternalAuthException(string message, int statusCode) : base(message)
         {
             StatusCode = statusCode;
