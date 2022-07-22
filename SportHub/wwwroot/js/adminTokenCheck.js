994749067e1295ae4262c5a36d50daf9dc16844e
likviq <p3tro.pavlyk@gmail.com>
2022-07-22 15:15:07 +0300

    only login users(admins) can use admin page(anothers are redirecting), implement switch button(навіть працює)

---
+++ SportHub/wwwroot/js/adminTokenCheck.js
@@ -0,0 +1,9 @@
+﻿var token = localStorage.getItem('Jwt Token');
+if (token) {
+    var parseToken = JSON.parse(atob(token.split('.')[1]));
+}
+
+if (parseToken.role == "User") {
+    console.log("u a not an admin");
+    window.location.replace("/");
+}
