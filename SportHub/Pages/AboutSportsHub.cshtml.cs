c153a5a645ba9c5d87ddaf7a2d0430b5f9efa021
PavloYend <pavlo.yendyk.knm.2020@lpnu.ua>
2022-07-24 16:55:06 +0300

    add footer to user side layout

---
+++ SportHub/Pages/AboutSportsHub.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportHub.Pages
+{
+    public class AboutSportsHubModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
