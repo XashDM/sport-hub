60232d97ee87937ea8586b6e855f9f629566ddc7
likviq <p3tro.pavlyk@gmail.com>
2022-07-25 21:09:49 +0300

    resolve problems with exp token and unauthorized user

--- SportHub/wwwroot/js/adminTokenCheck.js
+++ SportHub/wwwroot/js/adminTokenCheck.js
@@ -2,7 +2,13 @@
 if (token) {
     var parseToken = JSON.parse(atob(token.split('.')[1]));
 }
+else {
+    window.location.replace("/");
+}
 
-if (!parseToken.role.includes("Admin")) {
+console.log(parseToken);
+var today = new Date();
+var currentTime = today.getTime() / 1000 >> 0;
+if (!parseToken.role.includes("Admin") || parseToken.exp < currentTime) {
     window.location.replace("/");
 }
