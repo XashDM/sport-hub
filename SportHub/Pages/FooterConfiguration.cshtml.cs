ec9774118c86cf37917048c3034004043aa1d049
PavloYend <pavlo.yendyk.knm.2020@lpnu.ua>
2022-07-29 13:38:57 +0300

    add adminFooterPage

---
+++ SportHub/Pages/FooterConfiguration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportHub.Pages
+{
+    public class FooterConfigurationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SportHub/wwwroot/js/FooterConfiguration.js
@@ -0,0 +1,10 @@
+﻿/*var saveButton = document.getElementById('button1')
+var inputHeadline = document.getElementById('input1')
+var headlineText = document.getElementById('head1')
+
+saveButton.onclick = function () {
+    localStorage.setItem('headline', inputHeadline.value);
+    headlineText.textContent = localStorage.getItem('headline')
+}*/
+
+
